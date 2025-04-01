0f36d18f05eb857fb76e2dde6ae58dcd54400972
Mikołaj Machowski <mmachowski@mnw.art.pl>
2025-04-01 09:14:39 +0000

    add deploy sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#
+HOST=$(hostname)
+# copy php files
+
+if [[ $HOST == "ojs" ]]; then
+	cp -vf *.php /var/www/html/guide
+	# copy data file
+	cp -vf data.csv /var/www/html/guide
+	# copy assets (aux files, graphics?, css)
+	cp -vRf assets/*.css assets/*.ico /var/www/html/guide/assets/
+fi
