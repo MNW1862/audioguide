fb7a0a486cd9410db442c45080ae1d2b6b22a5f9
Mikołaj Machowski <mmachowski@mnw.art.pl>
2025-04-30 14:11:51 +0000

    clear remnants of overlay menu

--- index.php
+++ index.php
@@ -19,50 +19,47 @@ require '/var/www/include_audio/common.php';
     <!-- Hamburger button -->
     <a class="menu-icon" href="menu.php<?php echo "?lang=" . $lang['lang_ver']; ?>">
     <svg viewBox="0 0 30 30" fill="none" stroke="black" stroke-width="2">
-	<title id="open_menu"><?php echo $lang['index_menu_open']; ?></title>
-	<line x1="3" y1="6" x2="21" y2="6" />
-	<line x1="3" y1="12" x2="21" y2="12" />
-	<line x1="3" y1="18" x2="21" y2="18" />
+    <title id="open_menu"><?php echo $lang['index_menu_open']; ?></title>
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <line x1="3" y1="12" x2="21" y2="12" />
+    <line x1="3" y1="18" x2="21" y2="18" />
     </svg>
     </a>
     </header>
-  <!-- Overlay -->
 
-        
-        <form action="process.php" method="get" onsubmit="return validateForm()">
-            <input type="text" id="number" name="number" placeholder="123" tabindex="0" required readonly>
-	    <input type="hidden" id="lang" name="lang" value="<?php echo $lang['lang_ver']; ?>">
-            <label for="number">
-            <p id="error-message" class="error-message"><?php echo $lang['index_error_message']; ?></p>
-            </label>
-            
-            <!-- Custom Numeric Keypad -->
-            <div class="keypad" role="keypad">
-                <button type="button" tabindex="1" onclick="addDigit('1')">1</button>
-                <button type="button" tabindex="2" onclick="addDigit('2')">2</button>
-                <button type="button" tabindex="3" onclick="addDigit('3')">3</button>
-                
-                <button type="button" tabindex="4" onclick="addDigit('4')">4</button>
-                <button type="button" tabindex="5" onclick="addDigit('5')">5</button>
-                <button type="button" tabindex="6" onclick="addDigit('6')">6</button>
-                
-                <button type="button" tabindex="7" onclick="addDigit('7')">7</button>
-                <button type="button" tabindex="8" onclick="addDigit('8')">8</button>
-                <button type="button" tabindex="9" onclick="addDigit('9')">9</button>
-                
-                <button type="button" onclick="clearInput()" class="clear-btn" title="<?php echo $lang['index_clear_no']; ?>">C</button>
-                <button type="button" tabindex="10" onclick="addDigit('0')">0</button>
-                <button type="button" tabindex="11" onclick="deleteLastDigit()" class="backspace-btn" title="<?php echo $lang['index_delete_last']; ?>">&#x232b;</button>
-            </div>
+    <form action="process.php" method="get" onsubmit="return validateForm()">
+    <input type="text" id="number" name="number" placeholder="123" tabindex="0" required readonly>
+    <input type="hidden" id="lang" name="lang" value="<?php echo $lang['lang_ver']; ?>">
+    <label for="number">
+    <p id="error-message" class="error-message"><?php echo $lang['index_error_message']; ?></p>
+    </label>
 
-            <button type="submit" title="<?php echo $lang['index_send']; ?>">&#x2b95;</button>
-        </form>
+    <!-- Custom Numeric Keypad -->
+    <div class="keypad" role="keypad">
+        <button type="button" tabindex="1" onclick="addDigit('1')">1</button>
+        <button type="button" tabindex="2" onclick="addDigit('2')">2</button>
+        <button type="button" tabindex="3" onclick="addDigit('3')">3</button>
+
+        <button type="button" tabindex="4" onclick="addDigit('4')">4</button>
+        <button type="button" tabindex="5" onclick="addDigit('5')">5</button>
+        <button type="button" tabindex="6" onclick="addDigit('6')">6</button>
+
+        <button type="button" tabindex="7" onclick="addDigit('7')">7</button>
+        <button type="button" tabindex="8" onclick="addDigit('8')">8</button>
+        <button type="button" tabindex="9" onclick="addDigit('9')">9</button>
+
+        <button type="button" onclick="clearInput()" class="clear-btn" title="<?php echo $lang['index_clear_no']; ?>">C</button>
+        <button type="button" tabindex="10" onclick="addDigit('0')">0</button>
+        <button type="button" tabindex="11" onclick="deleteLastDigit()" class="backspace-btn" title="<?php echo $lang['index_delete_last']; ?>">&#x232b;</button>
     </div>
+
+    <button type="submit" title="<?php echo $lang['index_send']; ?>">&#x2b95;</button>
+    </form>
 
     <script nonce="<?php echo $nonce_string; ?>">
         function addDigit(digit) {
             var inputField = document.getElementById('number');
-            
+
             if (inputField.value.length < 6) {
                 inputField.value += digit;
             }
